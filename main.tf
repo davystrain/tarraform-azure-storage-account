b4b49b61b3289d702cb84ebe7f10cc946c92c0ae
davy <davy.strain@hotmail.com>
2025-07-05 16:04:33 +0800

    chnaged variable names

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    }
-  }
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.name
   location = var.location

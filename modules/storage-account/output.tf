c815d68ab3c999921c223f415b6ab732003b190c
davy <davy.strain@hotmail.com>
2025-07-06 20:01:08 +0800

    added provider block

--- modules/storage-account/output.tf
+++ modules/storage-account/output.tf
@@ -1,9 +1,9 @@
-output "storage_queues" {
-  description = "The storage queues created."
-  value       = azurerm_storage_queue.this[*].name
-}
+# output "storage_queues" {
+#   description = "The storage queues created."
+#   value       = azurerm_storage_queue.this[*].name
+# }
 
-output "storage_tables" {
-  description = "The storage tables created."
-  value       = azurerm_storage_table.this[*].name
-}
+# output "storage_tables" {
+#   description = "The storage tables created."
+#   value       = azurerm_storage_table.this[*].name
+# }

61ce76d64c0e8c9af6586f759f9437e179347b17
David Strain <david.strain@rac.com.au>
2025-07-14 16:24:02 +0800

    added new config for data driven terraform module

--- modules/yaml-storage-account/variables.tf
+++ modules/yaml-storage-account/variables.tf
@@ -2,3 +2,7 @@ variable "yaml_config_path" {
   description = "Path to the YAML configuration file."
   type        = string
 }
+variable "resource_group_name" {
+  description = "The name of the resource group where the storage account is located."
+  type        = string
+}

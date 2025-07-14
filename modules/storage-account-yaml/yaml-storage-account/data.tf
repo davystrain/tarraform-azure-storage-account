571113a0f080ced8cd3efac60917d59741336a10
David Strain <david.strain@rac.com.au>
2025-07-14 16:53:55 +0800

    added new config for data driven terraform module

--- modules/storage-account-yaml/yaml-storage-account/data.tf
+++ modules/storage-account-yaml/yaml-storage-account/data.tf
@@ -1,4 +1,4 @@
 data "azurerm_resource_group" "rg" {
-  for_each = { for sa in local.storage_account_list : sa.resource_group_name => sa }
+  for_each = { for rg in local.storage_account_list : rg => rg }
   name     = each.key
 }

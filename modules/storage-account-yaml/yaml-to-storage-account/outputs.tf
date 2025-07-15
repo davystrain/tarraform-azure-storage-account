88208b1c71626f0e50549b83d5b1d1fb2d892dee
David Strain <david.strain@rac.com.au>
2025-07-15 20:30:47 +0800

    added code for role assignment

--- modules/storage-account-yaml/yaml-to-storage-account/outputs.tf
+++ modules/storage-account-yaml/yaml-to-storage-account/outputs.tf
@@ -2,3 +2,8 @@ output "storage_account_map" {
   description = "Map of storage account name to storage account object."
   value       = local.storage_account_map
 }
+
+output "container_role_assignments" {
+  value = local.container_role_assignments
+  description = "Flattened list of container role assignments extracted from YAML"
+}

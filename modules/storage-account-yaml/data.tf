270f6133751a418118f6b73d927127e285000cf4
David Strain <david.strain@rac.com.au>
2025-07-16 09:46:14 +0800

    handling duplicate role assignments

--- modules/storage-account-yaml/data.tf
+++ modules/storage-account-yaml/data.tf
@@ -6,25 +6,24 @@ data "azurerm_resource_group" "rg" {
 
 data "azuread_user" "users" {
   for_each = {
-    for ra in var.container_role_assignments : "${ra.principal_type}-${ra.principal_name}" => ra
-    if ra.principal_type == "User"
+    for k, ras in groupby(var.container_role_assignments, [ra -> "${ra.principal_type}-${ra.principal_name}"]) :
+    k => ras[0] if ras[0].principal_type == "User"
   }
   user_principal_name = each.value.principal_name
 }
 
 data "azuread_group" "groups" {
   for_each = {
-    for ra in var.container_role_assignments : "${ra.principal_type}-${ra.principal_name}" => ra
-    if ra.principal_type == "Group"
+    for k, ras in groupby(var.container_role_assignments, [ra -> "${ra.principal_type}-${ra.principal_name}"]) :
+    k => ras[0] if ras[0].principal_type == "Group"
   }
-
   display_name = each.value.principal_name
 }
 
 data "azuread_service_principal" "sps" {
   for_each = {
-    for ra in var.container_role_assignments : "${ra.principal_type}-${ra.principal_name}" => ra
-    if ra.principal_type == "ServicePrincipal"
+    for k, ras in groupby(var.container_role_assignments, [ra -> "${ra.principal_type}-${ra.principal_name}"]) :
+    k => ras[0] if ras[0].principal_type == "ServicePrincipal"
   }
   display_name = each.value.principal_name
 }

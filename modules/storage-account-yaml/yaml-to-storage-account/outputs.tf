796287d1a03b6996be1ee58f6c777b5d03590503
David Strain <david.strain@rac.com.au>
2025-07-16 08:26:55 +0800

    handling duplicate role assignments

--- modules/storage-account-yaml/yaml-to-storage-account/outputs.tf
+++ modules/storage-account-yaml/yaml-to-storage-account/outputs.tf
@@ -7,3 +7,7 @@ output "container_role_assignments" {
   value = local.container_role_assignments
   description = "Flattened list of container role assignments extracted from YAML"
 }
+
+output "container_role_assignments_map" {
+  value = local.container_role_assignments_map
+}

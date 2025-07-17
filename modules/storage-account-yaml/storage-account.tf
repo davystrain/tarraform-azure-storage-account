419f84950ed4829747afaed6366db36b1d81d8c9
David Strain <david.strain@rac.com.au>
2025-07-17 15:02:43 +0800

    added storage account depends on

--- modules/storage-account-yaml/storage-account.tf
+++ modules/storage-account-yaml/storage-account.tf
@@ -89,10 +89,6 @@ resource "azurerm_role_assignment" "container_roles" {
   scope                = each.value.scope
   role_definition_name = each.value.role_definition_name
   principal_id         = each.value.principal_id
-
-  depends_on = [
-    azurerm_storage_container.reusable_module
-  ]
 }
 
 

44ab60cc98d5522d52a44203e0ef02387ac0be6a
David Strain <david.strain@rac.com.au>
2025-07-17 20:47:26 +0800

    added depends on

--- modules/storage-account-yaml/storage-account.tf
+++ modules/storage-account-yaml/storage-account.tf
@@ -84,17 +84,9 @@ resource "azurerm_storage_container" "reusable_module" {
 # }
 
 resource "azurerm_role_assignment" "container_roles" {
-  for_each = local.container_role_assignment_objects
+  for_each = local.container_role_assignment_map
 
-  scope                = azurerm_storage_container.reusable_module[each.value.container_name].resource_manager_id
+  scope                = each.value.scope
   role_definition_name = each.value.role_definition_name
-  principal_id = (
-    each.value.principal_type == "User" ? data.azuread_user.users[each.value.principal_name].object_id :
-    each.value.principal_type == "Group" ? data.azuread_group.groups[each.value.principal_name].object_id :
-    each.value.principal_type == "ServicePrincipal" ? data.azuread_service_principal.sps[each.value.principal_name].object_id :
-    null
-  )
-
-  depends_on = [azurerm_storage_container.reusable_module]
+  principal_id         = each.value.principal_id
 }
-

29d3842d22fad2dbefcfac5396a63b572fb735d1
David Strain <david.strain@rac.com.au>
2025-07-18 08:55:31 +0800

    added edits to create role assignments

--- modules/storage-account-yaml/locals.tf
+++ modules/storage-account-yaml/locals.tf
@@ -1,17 +1,9 @@
 locals {
-  container_role_assignment_map = {
+  container_role_assignment_objects = {
     for ra in var.container_role_assignments :
-    "${ra.principal_type}-${ra.principal_name}-${ra.role_definition_name}-${ra.container_name}" => {
-      scope                = azurerm_storage_container.reusable_module[ra.container_name].resource_manager_id
-      role_definition_name = ra.role_definition_name
-      principal_id = (
-        ra.principal_type == "User" ? data.azuread_user.users[ra.principal_name].object_id :
-        ra.principal_type == "Group" ? data.azuread_group.groups[ra.principal_name].object_id :
-        ra.principal_type == "ServicePrincipal" ? data.azuread_service_principal.sps[ra.principal_name].object_id :
-        null
-      )
-    }
+    "${ra.principal_type}-${ra.principal_name}-${ra.role_definition_name}-${ra.container_name}" => ra
   }
+
   group_names = toset([
     for ra in var.container_role_assignments : ra.principal_name
     if ra.principal_type == "Group"

7e49b9ea807e441c1f3346994c9da719ea0f5ec0
David Strain <david.strain@rac.com.au>
2025-07-21 11:08:59 +0800

    removed outputs from root folder

--- modules/storage-account-yaml/output.tf
+++ modules/storage-account-yaml/output.tf
@@ -1,13 +1,4 @@
-output "storage_account_id" {
-  description = "The ID of the storage account."
-  value       = azurerm_storage_account.reusable_module.id
-}
-
 output "storage_container_ids" {
   description = "Map of storage container names to their IDs."
   value       = { for c in azurerm_storage_container.reusable_module : c.name => c.id }
-}
-
-output "resource_group_location" {
-  value = data.azurerm_resource_group.rg.location
 }

8817306ce13ce784207217f6fe9aed32a7be1e51
David Strain <david.strain@rac.com.au>
2025-07-21 16:33:00 +0800

    adding to one module call

--- modules/storage-account-yaml/yaml-to-storage-account/variables.tf
+++ modules/storage-account-yaml/yaml-to-storage-account/variables.tf
@@ -2,8 +2,3 @@ variable "yaml_config_path" {
   description = "Path to the YAML configuration file."
   type        = string
 }
-
-variable "storage_container_ids" {
-  description = "Map of container names to their resource IDs"
-  type        = map(string)
-}

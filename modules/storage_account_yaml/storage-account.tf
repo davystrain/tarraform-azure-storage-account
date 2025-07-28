be6ba25bb181d363e70a5dbc94b4584200ba61b0
David Strain <david.strain@rac.com.au>
2025-07-28 19:55:58 +0800

    fixing error

--- modules/storage_account_yaml/storage-account.tf
+++ modules/storage_account_yaml/storage-account.tf
@@ -1,30 +1,51 @@
 resource "azurerm_storage_account" "reusable_module" {
-  name                             = var.name
-  resource_group_name              = data.azurerm_resource_group.rg.name
-  location                         = data.azurerm_resource_group.rg.location
-  access_tier                      = var.access_tier
-  account_replication_type         = var.account_replication_type
-  account_tier                     = var.account_tier
-  account_kind                     = var.account_kind
-  allow_nested_items_to_be_public  = var.allow_nested_items_to_be_public
-  cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
-  default_to_oauth_authentication  = var.default_to_oauth_authentication
-  https_traffic_only_enabled       = var.https_traffic_only_enabled
-  min_tls_version                  = var.min_tls_version
-  public_network_access_enabled    = var.public_network_access_enabled
-  shared_access_key_enabled        = var.shared_access_key_enabled
-  local_user_enabled               = var.local_user_enabled
-  tags                             = merge(data.azurerm_resource_group.rg.tags, var.tags)
+  for_each = local.storage_account_map
+
+  name                             = each.value.storage_account_name
+  resource_group_name              = data.azurerm_resource_group.rg[each.value.resource_group_name].name
+  location                         = each.value.location
+  access_tier                      = each.value.access_tier
+  account_replication_type         = each.value.account_replication_type
+  account_tier                     = each.value.account_tier
+  account_kind                     = each.value.account_kind
+  allow_nested_items_to_be_public  = each.value.allow_nested_items_to_be_public
+  cross_tenant_replication_enabled = each.value.cross_tenant_replication_enabled
+  default_to_oauth_authentication  = each.value.default_to_oauth_authentication
+  https_traffic_only_enabled       = each.value.https_traffic_only_enabled
+  min_tls_version                  = each.value.min_tls_version
+  public_network_access_enabled    = each.value.public_network_access_enabled
+  shared_access_key_enabled        = each.value.shared_access_key_enabled
+  local_user_enabled               = each.value.local_user_enabled
+  tags                             = merge(data.azurerm_resource_group.rg[each.value.resource_group_name].tags, each.value.tags)
 }
 
 resource "azurerm_storage_container" "reusable_module" {
-  for_each           = { for c in var.containers : c.name => c }
-  name               = each.value.name
-  storage_account_id = azurerm_storage_account.reusable_module.id
+  for_each = {
+    for key, container in flatten([
+      for sa_name, sa_config in local.storage_account_map : [
+        for container in sa_config.containers : {
+          key                  = "${sa_name}-${container.name}"
+          storage_account_name = sa_name
+          container_name       = container.name
+        }
+      ]
+    ]) : container.key => container
+  }
+
+  name               = each.value.container_name
+  storage_account_id = azurerm_storage_account.reusable_module[each.value.storage_account_name].id
 }
 
 resource "azurerm_role_assignment" "container_roles" {
-  for_each = local.container_role_assignments
+  for_each = {
+    for key, ra in local.container_role_assignments : key => merge(ra, {
+      scope = "${azurerm_storage_account.reusable_module[ra.storage_account_name].id}/blobServices/default/containers/${ra.container_name}"
+      principal_id = ra.principal_type == "User" ? data.azuread_user.users[ra.principal_name].object_id : (
+        ra.principal_type == "Group" ? data.azuread_group.groups[ra.principal_name].object_id : 
+        data.azuread_service_principal.sps[ra.principal_name].object_id
+      )
+    })
+  }
 
   scope                = each.value.scope
   role_definition_name = each.value.role_definition_name
@@ -33,19 +54,42 @@ resource "azurerm_role_assignment" "container_roles" {
 }
 
 resource "azurerm_storage_queue" "reusable_module" {
-  for_each             = { for q in var.queues : q.name => q }
-  name                 = each.value.name
-  storage_account_name = azurerm_storage_account.reusable_module.name
-  metadata             = try(each.value.metadata, {})
+  for_each = {
+    for key, queue in flatten([
+      for sa_name, sa_config in local.storage_account_map : [
+        for queue in sa_config.queues : {
+          key                  = "${sa_name}-${queue.name}"
+          storage_account_name = sa_name
+          queue_name           = queue.name
+          metadata             = try(queue.metadata, {})
+        }
+      ]
+    ]) : queue.key => queue
+  }
+
+  name                 = each.value.queue_name
+  storage_account_name = azurerm_storage_account.reusable_module[each.value.storage_account_name].name
+  metadata             = each.value.metadata
 }
 
-# UPDATED: Tables using for_each
 resource "azapi_resource" "reusable_module_table" {
-  for_each  = { for t in var.tables : t.name => t }
+  for_each = {
+    for key, table in flatten([
+      for sa_name, sa_config in local.storage_account_map : [
+        for table in sa_config.tables : {
+          key                  = "${sa_name}-${table.name}"
+          storage_account_name = sa_name
+          table_name           = table.name
+          properties           = try(table.properties, {})
+        }
+      ]
+    ]) : table.key => table
+  }
+
   type      = "Microsoft.Storage/storageAccounts/tableServices/tables@2022-09-01"
-  name      = each.value.name
-  parent_id = "${azurerm_storage_account.reusable_module.id}/tableServices/default"
+  name      = each.value.table_name
+  parent_id = "${azurerm_storage_account.reusable_module[each.value.storage_account_name].id}/tableServices/default"
   body = {
-    properties = try(each.value.properties, {})
+    properties = each.value.properties
   }
 }

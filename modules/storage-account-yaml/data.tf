fe29884fb3de0e75f4342ac9b32e3e1e9b3ed092
David Strain <david.strain@rac.com.au>
2025-07-29 09:10:31 +0800

    removed config yaml path

--- modules/storage-account-yaml/data.tf
+++ modules/storage-account-yaml/data.tf
@@ -5,29 +5,17 @@ data "azurerm_resource_group" "rg" {
 }
 
 data "azuread_user" "users" {
-  for_each = toset([
-    for ra in var.container_role_assignments : ra.principal_name
-    if ra.principal_type == "User"
-  ])
-  user_principal_name = each.key
+  for_each            = local.user_principals  # Uses the local we created
+  user_principal_name = each.value
 }
-
 
 data "azuread_group" "groups" {
-  for_each = toset([
-    for ra in var.container_role_assignments : ra.principal_name
-    if ra.principal_type == "Group"
-  ])
-  display_name = each.key
+  for_each     = local.group_principals  # Uses the local we created
+  display_name = each.value
 }
-
 
 data "azuread_service_principal" "sps" {
-  for_each = toset([
-    for ra in var.container_role_assignments : ra.principal_name
-    if ra.principal_type == "ServicePrincipal"
-  ])
-  display_name = each.key
+  for_each     = local.sp_principals  # Uses the local we created
+  display_name = each.value
 }
-
 
--- modules/storage-account-yaml/outputs.tf
+++ modules/storage-account-yaml/outputs.tf
@@ -1,8 +1,3 @@
-output "storage_account_map" {
-  description = "Map of storage account name to storage account object."
-  value       = local.storage_account_map
-}
-
 output "storage_container_ids" {
   description = "Map of storage container names to their IDs."
   value       = { for c in azurerm_storage_container.reusable_module : c.name => c.id }

81ca5787a3c73c441a9aae82151a3de74878e784
David Strain <david.strain@rac.com.au>
2025-07-29 12:25:50 +0800

    removed config yaml path

--- modules/storage-account-yaml/outputs.tf
+++ modules/storage-account-yaml/outputs.tf
@@ -1,4 +1,4 @@
-output "storage_container_ids" {
-  description = "Map of storage container names to their IDs."
-  value       = { for c in azurerm_storage_container.sc : c.name => c.id }
-}
+# output "storage_container_ids" {
+#   description = "Map of storage container names to their IDs."
+#   value       = { for c in azurerm_storage_container.sc : c.name => c.id }
+# }

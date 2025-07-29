e743cf3a48c17ef18c1418ee5fdbccd97d4d4497
David Strain <david.strain@rac.com.au>
2025-07-29 14:56:13 +0800

    changed outputs

--- modules/storage-account-yaml/outputs.tf
+++ modules/storage-account-yaml/outputs.tf
@@ -2,3 +2,7 @@ output "storage_account_id" {
   description = "The ID of the storage account."
   value       = azurerm_storage_account.sa.id
 }
+output "storage_account_name" {
+  description = "The name of the storage account."
+  value       = azurerm_storage_account.sa.name
+}

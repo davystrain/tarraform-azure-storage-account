942a99a7dce9bd8231a7acacd43bef643038d2ef
David Strain <david.strain@rac.com.au>
2025-07-30 15:50:46 +0800

    adding back network rules

--- modules/standard-private-only/locals.tf
+++ modules/standard-private-only/locals.tf
@@ -15,21 +15,6 @@ locals {
     ]
   ])
 
-  # Create role assignments map for azurerm_role_assignment resources
-  container_role_assignments = {
-    for ra in local.flattened_container_role_assignments :
-    "${ra.principal_type}-${ra.principal_name}-${ra.role_definition_name}-${ra.container_name}" => {
-      scope                = azurerm_storage_container.sc[ra.container_name].resource_manager_id
-      role_definition_name = ra.role_definition_name
-      principal_id = (
-        ra.principal_type == "User" ? data.azuread_user.users[ra.principal_name].object_id :
-        ra.principal_type == "Group" ? data.azuread_group.groups[ra.principal_name].object_id :
-        ra.principal_type == "ServicePrincipal" ? data.azuread_service_principal.sps[ra.principal_name].object_id :
-        null
-      )
-    }
-  }
-
   # Extract unique principal names for data source lookups
   user_principals = toset([
     for ra in local.flattened_container_role_assignments : ra.principal_name
@@ -45,4 +30,19 @@ locals {
     for ra in local.flattened_container_role_assignments : ra.principal_name
     if ra.principal_type == "ServicePrincipal"
   ])
+
+  # Create role assignments map for azurerm_role_assignment resources
+  container_role_assignments = {
+    for ra in local.flattened_container_role_assignments :
+    "${ra.principal_type}-${ra.principal_name}-${ra.role_definition_name}-${ra.container_name}" => {
+      scope                = azurerm_storage_container.sc[ra.container_name].resource_manager_id
+      role_definition_name = ra.role_definition_name
+      principal_id = (
+        ra.principal_type == "User" ? data.azuread_user.users[ra.principal_name].object_id :
+        ra.principal_type == "Group" ? data.azuread_group.groups[ra.principal_name].object_id :
+        ra.principal_type == "ServicePrincipal" ? data.azuread_service_principal.sps[ra.principal_name].object_id :
+        null
+      )
+    }
+  }
 }

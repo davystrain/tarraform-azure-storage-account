173318af2c76eee23fd6b0933af2ec8c4d2f660a
David Strain <david.strain@rac.com.au>
2025-07-31 12:32:16 +0800

    editing module calls

--- modules/standard-private-only/storage-account.tf
+++ modules/standard-private-only/storage-account.tf
@@ -64,8 +64,8 @@ resource "azurerm_private_endpoint" "blob" {
   count = length(var.containers) > 0 ? 1 : 0
 
   name                          = "${azurerm_storage_account.sa.name}-pe1"
-  location                      = data.azurerm_resource_group.private_endpoint.location
-  resource_group_name           = data.azurerm_resource_group.private_endpoint.name
+  location                      = data.azurerm_resource_group.rg.location
+  resource_group_name           = data.azurerm_resource_group.rg.name
   subnet_id                     = data.azurerm_subnet.private_endpoint_subnet.id
   custom_network_interface_name = "pe1-${var.name}"
 

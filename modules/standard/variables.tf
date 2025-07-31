5b992d4aa05ff943523a21533a38b77b00609a79
David Strain <david.strain@rac.com.au>
2025-07-31 16:10:46 +0800

    refactoring modules

--- modules/standard/variables.tf
+++ modules/standard/variables.tf
@@ -93,12 +93,7 @@ variable "network_rules" {
     ip_rules                   = list(string)
     virtual_network_subnet_ids = list(string)
   })
-  default = {
-    bypass                     = ["AzureServices"]
-    default_action             = "Allow"
-    ip_rules                   = []
-    virtual_network_subnet_ids = []
-  }
+  default = {}
 }
 
 variable "containers" {

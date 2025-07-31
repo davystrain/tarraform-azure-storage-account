92347f98858e4e3439eb4ef678f833193aa9e9d6
David Strain <david.strain@rac.com.au>
2025-07-31 12:54:12 +0800

    refactoring modules

--- modules/standard/output.tf
+++
@@ -1,9 +0,0 @@
-output "storage_account_id" {
-  description = "The ID of the storage account."
-  value       = azurerm_storage_account.reusable_module.id
-}
-
-output "storage_account_name" {
-  description = "The name of the storage account."
-  value       = azurerm_storage_account.reusable_module.name
-}

ab64f0194ce0e103bbe8708a566b10e02b328c5b
David Strain <david.strain@rac.com.au>
2025-08-03 17:17:12 +0800

    private endpoint queue depends on

--- modules/standard-private-only/storage-account.tf
+++ modules/standard-private-only/storage-account.tf
@@ -42,11 +42,21 @@ resource "azurerm_role_assignment" "container_roles" {
   depends_on           = [azurerm_storage_account.sa, azurerm_storage_container.sc]
 }
 
-resource "azurerm_storage_queue" "sq" {
+# resource "azurerm_storage_queue" "sq" {
+#   for_each             = { for q in var.queues : q.name => q }
+#   name                 = each.value.name
+#   storage_account_name = azurerm_storage_account.sa.name
+#   # depends_on = [ azurerm_private_endpoint.queue ]
+# }
+
+resource "azapi_resource" "queue" {
   for_each             = { for q in var.queues : q.name => q }
+  type                 = "Microsoft.Storage/storageAccounts/queueServices/queues@2022-09-01"
   name                 = each.value.name
-  storage_account_name = azurerm_storage_account.sa.name
-  # depends_on = [ azurerm_private_endpoint.queue ]
+  parent_id            = "${azurerm_storage_account.sa.id}/queueServices/default"
+  body = {
+    properties = try(each.value.properties, {})
+  }
 }
 
 # UPDATED: Tables using for_each

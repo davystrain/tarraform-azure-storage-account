941c7a65ab2b2c2cdd79c3df88647c4b7b994520
David Strain <david.strain@rac.com.au>
2025-08-18 14:10:50 +0800

    added provider to private dns zones

--- modules/standard-private-only/data.tf
+++ modules/standard-private-only/data.tf
@@ -35,16 +35,16 @@ data "azurerm_private_dns_zone" "privatelink_blob_azure_net" {
   resource_group_name = "terraform"
 }
 
-data "azurerm_private_dns_zone" "privatelink_queue_azure_net" {
-  provider            = azurerm.pe-dns-infra
-  name                = "privatelink.queue.core.windows.net"
-  resource_group_name = "terraform"
-}
+# data "azurerm_private_dns_zone" "privatelink_queue_azure_net" {
+#   provider            = azurerm.pe-dns-infra
+#   name                = "privatelink.queue.core.windows.net"
+#   resource_group_name = "terraform"
+# }
 
-data "azurerm_private_dns_zone" "privatelink_table_azure_net" {
-  provider            = azurerm.pe-dns-infra
-  name                = "privatelink.table.core.windows.net"
-  resource_group_name = "terraform"
+# data "azurerm_private_dns_zone" "privatelink_table_azure_net" {
+#   provider            = azurerm.pe-dns-infra
+#   name                = "privatelink.table.core.windows.net"
+#   resource_group_name = "terraform"
 }
 
 data "azurerm_subnet" "private_endpoint_subnet" {

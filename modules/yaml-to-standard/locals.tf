c59479976aff52b67b6f3abff29c489bc1074b09
David Strain <david.strain@rac.com.au>
2025-08-20 10:40:04 +0800

    removed null value from local user enabled

--- modules/yaml-to-standard/locals.tf
+++ modules/yaml-to-standard/locals.tf
@@ -16,7 +16,7 @@ locals {
         min_tls_version                  = try(v.min_tls_version, null)
         public_network_access_enabled    = try(v.public_network_access_enabled, null)
         shared_access_key_enabled        = try(v.shared_access_key_enabled, null)
-        local_user_enabled               = try(v.local_user_enabled, null)
+        local_user_enabled               = try(v.local_user_enabled)
         network_rules                    = try(v.network_rules, null)
         containers                       = try(v.containers, [])
         queues                           = try(v.queues, [])
